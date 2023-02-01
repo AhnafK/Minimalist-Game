5c33d17cb3d2fedf998cf57dcd2948f7e5fb8744
William Yang <wy2037@nyu.edu>
2023-01-31 20:08:10 -0500

    Updated Par system + test merge

--- Minimalist Game/Assets/Scripts/Score.cs
+++ Minimalist Game/Assets/Scripts/Score.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Score : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI strikeText;
+    public TextMeshProUGUI parText;
+    int par;
     int currentScore = 0;
     int strike = 0;
+
+    void Start() {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Level 1") {
+            par = 5;
+        } else {
+            par = 10;
+        }
+        parText.text = "Par " + par;
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,7 +39,7 @@ public class Score : MonoBehaviour
     }
 
     public void calculateScore() {
-        currentScore += 1000 * (5 - strike);
+        currentScore += 1000 * (par - strike);
         // change 5 to par amount per course
         Debug.Log(currentScore);
     }
